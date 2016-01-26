bdf568c6c71de8834f1e1ea95061246c625e9ec3
EladNoy <EladNoyBio@gmail.com>
2016-01-26 11:23:13 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow for the repeated solving of matrices to commence in reduced time
+## by caching the results for each matrix solved for future requests.
 
-## Write a short comment describing this function
+## Input: A matrix
+## Output: A list including the input matrix and variables that assist in caching
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix()) 
+{
+        solved <- NULL
+        set <- function(y)
+		{
+                x <<- y
+                solved <<- NULL
+        }
+        get <- function() 
+		{return (x)}
+        set_solved <- function(solve) 
+		{solved <<- solve}
+        get_solved <- function() 
+		{return (solved)}
+        list(set = set, get = get,
+             set_solved = set_solved,
+             get_solved = get_solved)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+## Input: A list in which there is a matrix and caching assisting variables, as outputted by the ** makeCacheMatrix ** function
+## Output: The solution to the matrix; if it is cached, a cached solution is used to save time
+
+cacheSolve <- function(x, ...) 
+{
+        solved <- x$get_solved()
+        if(!is.null(solved)) 
+		{
+                message("getting cached data")
+                return(solved)
+        }
+        data <- x$get()
+        solved <- solve(data, ...)
+        x$set_solved(solved)
+        return (solved)
 }
+
